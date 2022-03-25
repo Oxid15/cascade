8c853ebb72503ee06e5d8403c7d3d1b1901a1223
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-03-25 18:46:33 +0300

    More classes from table imported

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .folder_image_dataset import FolderImageDataset
 from .sk_classifier import SkClassifier
-from .table_dataset import TableDataset
+from .table_dataset import TableDataset, CSVDataset, TableFilter
 from .text_classification_dataset import TextClassificationDataset
