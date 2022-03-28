9106842e86fa74c51c9860196edb2ab4a68c4940
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-03-28 23:07:56 +0300

    add oversampler to init

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .folder_image_dataset import FolderImageDataset
 from .sk_classifier import SkClassifier
 from .table_dataset import TableDataset, CSVDataset, TableFilter
 from .text_classification_dataset import TextClassificationDataset
+from .oversampler import OverSampler
