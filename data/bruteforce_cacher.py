3e85a0426178c3e41be0188848b386946c624336
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-04-02 21:41:04 +0300

    Make cacher support any interface

--- data/bruteforce_cacher.py
+++ data/bruteforce_cacher.py
@@ -1,11 +1,17 @@
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from . import Modifier, Wrapper, T
 
 
 class BruteforceCacher(Modifier):
     def __init__(self, dataset) -> None:
         super(BruteforceCacher, self).__init__(dataset)
-        self._data = [item for item in tqdm(self._dataset)] # forcibly calling all previous lazy loads
+        # forcibly calling all previous datasets in the init
+        if hasattr(self._dataset, '__len__'):
+            self._data = [self._dataset[i] for i in trange(len(self._dataset))]
+        elif hasattr(self._dataset, '__next__'):
+            self._data = [item for item in self._dataset]
+        else:
+            raise AttributeError(f'Input dataset must provide Listlike or Iterable interface')
 
     def __getitem__(self, index) -> T:
         return self._data[index]
