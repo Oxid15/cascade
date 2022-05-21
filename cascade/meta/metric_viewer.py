53ae2070a003481422b9b67ff639da36d2c6ad95
Ilia Moiseev <villeman.5@yandex.ru>
2022-05-21 23:29:17 +0300

    Change deprecated imports

--- cascade/meta/metric_viewer.py
+++ cascade/meta/metric_viewer.py
@@ -19,9 +19,7 @@ from plotly import graph_objects as go
 import pandas as pd
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash import Input, Output
+from dash import Input, Output, html, dcc
 
 from . import MetaViewer
 
