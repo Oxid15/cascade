0661125a0c1fbb693b5d710ca406fc0735cd1be6
Моисеев Илья <villeman.5@yandex.ru>
2022-06-23 13:30:29 +0500

    Update version

--- cascade/__init__.py
+++ cascade/__init__.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 __author__ = 'Ilia Moiseev'
 __author_email__ = 'ilia.moiseev.5@yandex.ru'
 
