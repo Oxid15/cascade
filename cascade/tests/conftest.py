f6875a5ea88af6246d348f5c276e4af4c002a424
Ilia Moiseev <villeman.5@yandex.ru>
2022-07-05 11:10:20 +0300

    Auto stash before cherry pick of "Close logging files at deletion of Repo"

---
+++ cascade/tests/conftest.py
@@ -0,0 +1,83 @@
+"""
+Copyright 2022 Ilia Moiseev
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import os
+import sys
+import random
+import shutil
+import numpy as np
+import pytest
+
+MODULE_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(MODULE_PATH))
+
+from cascade.models import Model, ModelRepo
+
+
+class DummyModel(Model):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = b'model'
+
+    def fit(self, *args, **kwargs):
+        pass
+
+    def predict(self, *args, **kwargs):
+        pass
+
+    def evaluate(self, *args, **kwargs):
+        self.metrics.update({'acc': np.random.random()})
+
+    def load(self, path):
+        if os.path.splitext(path)[-1] != '.bin':
+            path += '.bin'
+        with open(path, 'rb') as f:
+            self.model = str(f.read())
+
+    def save(self, path):
+        if os.path.splitext(path)[-1] != '.bin':
+            path += '.bin'
+        with open(path, 'wb') as f:
+            f.write(b'model')
+
+
+class EmptyModel(DummyModel):
+   def __init__(self):
+      self.metrics = {'acc': np.random.random()}
+
+@pytest.fixture(params=[
+   {'a': 0},
+   {'b': 1},
+   {'a': 0, 'b': 'alala'},
+   {'c': np.array([1, 2]), 'd': {'a': 0}}])
+def dummy_model(request):
+   return DummyModel(**request.param)
+
+
+@pytest.fixture
+def empty_model():
+   return EmptyModel()
+
+
+@pytest.fixture
+def model_repo():
+   yield ModelRepo('repo', lines=[
+        dict(
+            name=str(num),
+            cls=DummyModel) for num in range(10)
+        ])
+   shutil.rmtree('repo')
