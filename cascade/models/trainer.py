ec98b2d2483b638811c2fda90180066d9c434f65
Ilia Moiseev <villeman.5@yandex.ru>
2022-08-03 23:12:44 +0300

    Refactor, track time and metrics

--- cascade/models/trainer.py
+++ cascade/models/trainer.py
@@ -1,5 +1,9 @@
+from typing import Iterable, List, Dict
+
 from ..base import Traceable
 from ..models import Model, ModelRepo
+
+import pendulum
 
 
 class Trainer(Traceable):
@@ -15,17 +19,39 @@ class BasicTrainer(Trainer):
         super().__init__(*args, **kwargs)
 
         if isinstance(repo, str):
-            self.repo = ModelRepo(repo)
+            self._repo = ModelRepo(repo)
         elif isinstance(repo, ModelRepo):
-            self.repo = repo
+            self._repo = repo
+        
+        self.metrics = {}
 
-    def train(self, model: Model, train_data, 
-        test_data, train_kwargs, test_kwargs) -> None:
+    def train(self, 
+        model: Model, 
+        train_data: Iterable, 
+        test_data: Iterable, 
+        train_kwargs: Dict, 
+        test_kwargs: Dict,
+        line_name=None) -> None:
 
-        line_name = f'{len(self.repo):0>5d}'
-        self.repo.add_line(line_name, type(model))
+        if line_name is None:
+            line_name = f'{len(self._repo):0>5d}'
+        self._repo.add_line(line_name, type(model))
+
+        self.meta_prefix['train_start_time'] = pendulum.now()
 
         model.fit(train_data, **train_kwargs)
+
+        self.meta_prefix['train_end_time'] = pendulum.now()
+
         model.evaluate(test_data, **test_kwargs)
 
-        self.repo[line_name].save(model)
+        self.meta_prefix['evaluate_end_time'] = pendulum.now()
+
+        self._repo[line_name].save(model)
+
+        self.metrics = model.metrics
+        
+
+    def get_meta(self) -> List[Dict]:
+        meta = super().get_meta()
+        meta[-1]['metrics'] = self.metrics
