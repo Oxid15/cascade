76c80de11c2f968b95000c54e5443fe2e6e4df45
Ilia Moiseev <villeman.5@yandex.ru>
2022-09-01 23:41:40 +0300

    Add logging and train for a number of epochs

--- cascade/models/trainer.py
+++ cascade/models/trainer.py
@@ -1,9 +1,14 @@
-import pendulum
+import sys
+import logging
+from copy import deepcopy
 from typing import Iterable, List, Dict
 
+import pendulum
 from ..base import Traceable
 from ..models import Model, ModelRepo
 
+
+logger = logging.getLogger(__name__)
 
 class Trainer(Traceable):
     def __init__(self, repo, *args, **kwargs) -> None:
@@ -11,8 +16,10 @@ class Trainer(Traceable):
             self._repo = ModelRepo(repo)
         elif isinstance(repo, ModelRepo):
             self._repo = repo
-        
-        self.metrics = {}
+        else:
+            raise TypeError(f'Repo should be either ModelRepo or path, got {type(repo)}')
+
+        self.metrics = []
         super().__init__(*args, **kwargs)
 
     def train(self, model, *args, **kwargs):
@@ -30,27 +37,28 @@ class BasicTrainer(Trainer):
         super().__init__(repo, *args, **kwargs)
 
     def train(self, 
-        model: Model, 
-        train_data: Iterable, 
-        test_data: Iterable, 
-        train_kwargs: Dict, 
+        model: Model,
+        train_data: Iterable,
+        test_data: Iterable,
+        train_kwargs: Dict,
         test_kwargs: Dict,
+        epochs=1,
         line_name=None) -> None:
 
         if line_name is None:
             line_name = f'{len(self._repo):0>5d}'
         self._repo.add_line(line_name, type(model))
 
-        self._meta_prefix['train_start_time'] = pendulum.now()
+        self._meta_prefix['train_start_at'] = pendulum.now()
+        logger.info(f'Training started with parameters: {train_kwargs}')
+        logger.info(f'it will last {epochs} epochs')
 
-        model.fit(train_data, **train_kwargs)
+        for epoch in range(epochs):
+            model.fit(train_data, **train_kwargs)
+            model.evaluate(test_data, **test_kwargs)
+            self._repo[line_name].save(model)
+            self.metrics.append(deepcopy(model.metrics))
 
-        self._meta_prefix['train_end_time'] = pendulum.now()
+            logger.info(f'Epoch {epoch}: {model.metrics}')
 
-        model.evaluate(test_data, **test_kwargs)
-
-        self._meta_prefix['evaluate_end_time'] = pendulum.now()
-
-        self._repo[line_name].save(model)
-
-        self.metrics = model.metrics
+        self._meta_prefix['train_end_at'] = pendulum.now()
