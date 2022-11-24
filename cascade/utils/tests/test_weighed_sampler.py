baa2c9056f72b5937d46ca929317c7dfb128ac24
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-11-24 23:19:04 +0300

    Move weighed tests to utils where they belong

--- cascade/utils/tests/test_weighed_sampler.py
+++ cascade/utils/tests/test_weighed_sampler.py
@@ -17,7 +17,8 @@ limitations under the License.
 import os
 import sys
 
-MODULE_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+MODULE_PATH = os.path.dirname(
+    os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 sys.path.append(os.path.dirname(MODULE_PATH))
 
 import cascade.data as cdd
