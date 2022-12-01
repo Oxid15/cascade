773829a7d620f2837574aa62255d0a346622b32e
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-12-01 21:59:43 +0300

    Add test_data presence validation in trainer

--- cascade/models/trainer.py
+++ cascade/models/trainer.py
@@ -100,12 +100,13 @@ class BasicTrainer(Trainer):
                 If None - the strategy is 'save only meta'.
         """
 
-        # TODO: check if eval_strategy specified that test_data provided also
-
         if train_kwargs is None:
             train_kwargs = {}
         if test_kwargs is None:
             test_kwargs = {}
+
+        if eval_strategy is not None and test_data is None:
+            raise ValueError('Eval strategy is specified, but no test data provided')
 
         if start_from is not None:
             line_name = start_from
