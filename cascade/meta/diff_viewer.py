707092f27a533c678f8669a4f36f48a137506b94
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2023-02-14 23:31:27 +0300

    Draft very basic functionality

--- cascade/meta/diff_viewer.py
+++ cascade/meta/diff_viewer.py
@@ -1,21 +1,31 @@
 from typing import List, Any
-from ..models import ModelRepo
+from cascade.base import MetaFromFile
+from cascade.meta import MetaViewer
+from cascade.models import ModelRepo
 
 
 class DiffReader:
-    def read(self, repo: ModelRepo) -> List[Any]:
-        pass
+    def read_objects(self, path) -> List[MetaFromFile]:
+        return [{'a': 'b'}]
 
 
 class RepoDiffReader(DiffReader):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+
+    def read_objects(self, path) -> List[MetaFromFile]:
+        mev = MetaViewer(path, filt={'type': 'model'})
+        return [meta for meta in mev]
 
 
 class DiffViewer:
-    def __init__(self, repo) -> None:
-        pass
+    def __init__(self, path: str) -> None:
+        self._path = path
+        self._diff_reader = RepoDiffReader()
 
-    def serve(self, metric: str, **kwargs: Any) -> None:
+    def serve(self, **kwargs: Any) -> None:
+        objs = self._diff_reader.read_objects(self._path)
+
         # Conditional import
         try:
             import dash
@@ -25,32 +35,34 @@ class DiffViewer:
             dependency you can install it
             using the instructions from https://dash.plotly.com/installation''')
         else:
-            from dash import Input, Output, html, dcc
+            from dash import Input, Output, html, State, dcc
+            from dash_renderjson import DashRenderjson
 
         app = dash.Dash()
 
         app.layout = html.Div([
             html.H1(
-                children=f'DiffViewer',
+                children=f'DiffViewer in {self._path}',
                 style={
                     'textAlign': 'center',
                     'color': '#084c61',
                     'font-family': 'Montserrat'
                 }
             ),
-            # dcc.Graph(
-            #     id='history-figure',
-            #     figure=fig),
-            dcc.Interval(
-                id='history-interval',
-                interval=1000 * 3)
+            dcc.Dropdown(id='left-dropdown', options=[meta[0]['name'] for meta in objs]),
+            dcc.Dropdown(id='rigth-dropdown', options=[meta[0]['name'] for meta in objs]),
+
+            DashRenderjson(id=f'diff-json', data={'Nothing': 'Nothing is selected!'}),
+
+            html.Div(id='display', children=[
+                html.Details(children=[
+                    html.Summary(meta[0]['name']),
+                    DashRenderjson(id=f'data_{i}', data=meta)
+                ]) for i, meta in enumerate(objs)
+            ])
         ])
 
-        # @app.callback(Output('history-figure', 'figure'),
-        #               Input('history-interval', 'n_intervals'))
-        # def update_history(n_intervals):
-        #     self._repo.reload()
-        #     self._make_table()
-        #     return self.plot(metric)
-
         app.run_server(use_reloader=False, **kwargs)
+
+
+dv = DiffViewer(r'C:\cascade_integration\repos\demo').serve()
