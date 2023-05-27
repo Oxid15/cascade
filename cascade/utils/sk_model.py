2079db523508b47fd8d083839e568f397cc63f40
Ilia Moiseev <villeman.5@yandex.ru>
2023-05-27 23:59:56 +0300

    Formatting

--- cascade/utils/sk_model.py
+++ cascade/utils/sk_model.py
@@ -31,7 +31,10 @@ class SkModel(BasicModel):
     Accepts the name and block to form pipeline.
     Can fit, evaluate, predict save and load out of the box.
     """
-    def __init__(self, *args: Any, blocks: Union[List[Any], None] = None, **kwargs: Any) -> None:
+
+    def __init__(
+        self, *args: Any, blocks: Union[List[Any], None] = None, **kwargs: Any
+    ) -> None:
         """
         Parameters
         ----------
@@ -69,24 +72,24 @@ class SkModel(BasicModel):
 
     def _check_model_hash(self, path: str) -> None:
         root = os.path.dirname(path)
-        names = glob.glob(os.path.join(f'{root}', 'meta.*'))
+        names = glob.glob(os.path.join(f"{root}", "meta.*"))
         if len(names) == 1:
             meta = MetaHandler().read(names[0])
             # Uses first meta in list
             # Usually the list is of unit length
             meta = meta[0]
-            if 'md5sum' in meta:
-                with open(path, 'rb') as f:
+            if "md5sum" in meta:
+                with open(path, "rb") as f:
                     file_hash = md5(f.read()).hexdigest()
-                if file_hash != meta['md5sum']:
+                if file_hash != meta["md5sum"]:
                     raise RuntimeError(
-                        f'.pkl model hash check failed '
-                        f'it may be that model\'s .pkl file was corrupted\n'
+                        f".pkl model hash check failed "
+                        f"it may be that model's .pkl file was corrupted\n"
                         f'hash from {names[0]}: {meta["md5sum"]}\n'
-                        f'hash of {path}: {file_hash}'
+                        f"hash of {path}: {file_hash}"
                     )
         elif len(names) > 1:
-            raise RuntimeError(f'Multiple possible meta-files found: {names}')
+            raise RuntimeError(f"Multiple possible meta-files found: {names}")
 
     def load(self, path: str, check_hash: bool = True) -> None:
         """
@@ -95,13 +98,13 @@ class SkModel(BasicModel):
         if os.path.isdir(path):
             path = os.path.join(path, "model.pkl")
 
-        if os.path.splitext(path)[-1] != '.pkl':
-            path += '.pkl'
+        if os.path.splitext(path)[-1] != ".pkl":
+            path += ".pkl"
 
         if check_hash:
             self._check_model_hash(path)
 
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             self._pipeline = pickle.load(f)
 
     def save(self, path: str) -> None:
@@ -125,7 +128,5 @@ class SkModel(BasicModel):
 
     def get_meta(self) -> PipeMeta:
         meta = super().get_meta()
-        meta[0].update({
-            'pipeline': repr(self._pipeline)
-        })
+        meta[0].update({"pipeline": repr(self._pipeline)})
         return meta
