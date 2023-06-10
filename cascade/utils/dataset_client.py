33808067a69541dfd16cc28daa565321b6aac8a1
Ilia Moiseev <villeman.5@yandex.ru>
2023-06-10 21:38:33 +0300

    Write doc for client

--- cascade/utils/dataset_client.py
+++ cascade/utils/dataset_client.py
@@ -1,18 +1,33 @@
 import requests
+from typing import Any
 from ..data import Dataset
 
 
 class DatasetClient(Dataset):
+    """
+    Client for DatasetServer
+
+    See also
+    --------
+    cascade.utils.dataset_server.DatasetServer
+    """
+
     def __init__(self, host: str, **kwargs) -> None:
+        """
+        Parameters
+        ----------
+        host : str
+            URL of dataset server
+        """
         super().__init__(**kwargs)
         self._host = host
 
-    def __getitem__(self, idx):
-        res = requests.post(f"{self._host}/__getitem__", json={"idx": idx})
+    def __getitem__(self, idx: Any):
+        res = requests.post("/".join((self._host, "__getitem__")), json={"idx": idx})
         data = res.json()
         return data["item"]
 
     def __len__(self):
-        res = requests.post(f"{self._host}/__len__")
+        res = requests.get("/".join((self._host, "__len__")))
         data = res.json()
         return data["len"]
