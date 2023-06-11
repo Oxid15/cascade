57184fdf4d66a9e8547a81eca724f7cf30b2ae8e
Ilia Moiseev <villeman.5@yandex.ru>
2023-06-11 12:26:42 +0300

    Refactor and add getitem

--- cascade/utils/dataset_client.py
+++ cascade/utils/dataset_client.py
@@ -1,9 +1,11 @@
 import requests
 from typing import Any
+
+from ..base import PipeMeta
 from ..data import Dataset
 
 
-class DatasetClient:
+class DatasetClient(Dataset):
     """
     Client for DatasetServer
 
@@ -12,7 +14,7 @@ class DatasetClient:
     cascade.utils.dataset_server.DatasetServer
     """
 
-    def __init__(self, host: str, **kwargs) -> None:
+    def __init__(self, host: str, **kwargs: Any) -> None:
         """
         Parameters
         ----------
@@ -23,17 +25,19 @@ class DatasetClient:
         self._host = host
 
     def __getattribute__(self, name: str):
-        if name == "_build_method" or name == "_host":
+        if name == "_build_attr" or name == "_host":
             return object.__getattribute__(self, name)
-        return self._build_method(name)
+        return self._build_attr(name)
 
-    def _build_method(self, name: str):
-        def method(*args, **kwargs):
+    def _build_attr(self, name: str):
+        def attr(*args, **kwargs):
             res = requests.post(
                 self._host, json={"attr": name, "args": args, "kwargs": kwargs}
             )
-            print(res.text)
             data = res.json()
             return data["result"]
 
-        return method
+        return attr
+
+    def __getitem__(self, index: Any) -> Any:
+        return self._build_attr("__getitem__")(index)
