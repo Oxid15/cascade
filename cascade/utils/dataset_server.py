d666d20f204a69a11cf052ba433e692e6527a56c
Ilia Moiseev <villeman.5@yandex.ru>
2023-06-11 12:03:41 +0300

    Server now mirrors arbitrary calls to underlying object

--- cascade/utils/dataset_server.py
+++ cascade/utils/dataset_server.py
@@ -9,20 +9,15 @@ class DatasetServer(Modifier):
         super().__init__(dataset, **kwargs)
         self.app = Flask(__name__)
 
-        @self.app.route("/")
-        def get_meta():
-            return self.get_meta()
-
-        @self.app.route("/__getitem__", methods=["POST"])
-        def get_item(*args, **kwargs):
+        @self.app.route("/", methods=["POST"])
+        def method():
             req = request.json
-            data = self.__getitem__(req["idx"])
-            data = self._serialize(data)
-            return {"item": data}
+            attr = req.get("attr")
+            args = req.get("args")
+            kwargs = req.get("kwargs")
 
-        @self.app.route("/__len__", methods=["GET", "POST"])
-        def ln():
-            return {"len": self.__len__()}
+            attr = getattr(self._dataset, attr)
+            return {"result": self._serialize(attr(*args, **kwargs))}
 
     def _serialize(self, obj):
         return json.loads(JSONEncoder().encode(obj))
