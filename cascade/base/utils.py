8c75fc908d56cabe1a730e09bc0b864d63abb7af
Ilia Moiseev <villeman.5@yandex.ru>
2023-07-29 23:06:12 +0300

    Remove is_path

--- cascade/base/utils.py
+++ cascade/base/utils.py
@@ -1,4 +1,3 @@
-import os
 from coolname import generate
 
 
@@ -6,15 +5,3 @@ def generate_slug() -> str:
     words = generate(3)
     slug = "_".join(words)
     return slug
-
-
-def is_path(model: str) -> bool:
-    # Fast check for lines
-    if model.isnumeric():
-        return False
-
-    # os split will be crossplatform
-    if len(os.path.normpath(model).split(os.sep)) > 1:
-        return True
-    else:
-        return False
