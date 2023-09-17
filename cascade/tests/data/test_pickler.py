bcbbae4d915e183543d40d7bf16b75610260e304
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2023-09-17 23:57:42 +0300

    Separate tests by submodule

--- cascade/tests/data/test_pickler.py
+++ cascade/tests/data/test_pickler.py
@@ -19,7 +19,7 @@ import pytest
 MODULE_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(os.path.dirname(MODULE_PATH))
 
-from cascade.data import *
+from cascade.data import Pickler, Wrapper, Sampler, Modifier
 
 
 def test_data_coverage(dataset, tmp_path):
