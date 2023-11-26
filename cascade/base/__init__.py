61317f342c27087621dfe683a2ee0d34a871b1ad
Ilia Moiseev <villeman.5@yandex.ru>
2023-11-26 15:21:58 +0300

    Fix imports in base

--- cascade/base/__init__.py
+++ cascade/base/__init__.py
@@ -16,12 +16,6 @@ limitations under the License.
 
 from typing import Any, Dict, List, NoReturn, Union
 
-from .history_handler import HistoryHandler
-from .meta_handler import CustomEncoder as JSONEncoder
-from .meta_handler import MetaHandler, default_meta_format, supported_meta_formats
-from .traceable import Traceable, TraceableOnDisk
-
-
 """
 Single Meta of basic object is just a dict, however Cascade supports
 pipelines with lists of meta.
@@ -61,3 +55,8 @@ def raise_not_implemented(class_name: str, name: str) -> NoReturn:
         f"method called. May be you haven't "
         f"implemented it in the successor class"
     )
+
+from .history_handler import HistoryHandler
+from .meta_handler import CustomEncoder as JSONEncoder
+from .meta_handler import MetaHandler, default_meta_format, supported_meta_formats
+from .traceable import Traceable, TraceableOnDisk
