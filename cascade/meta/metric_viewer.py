7745cb006c2affc0dedd3d8c136864efef6ca45e
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-02-01 11:52:42 +0300

    Reorganize metric table around metrics instead of models

--- cascade/meta/metric_viewer.py
+++ cascade/meta/metric_viewer.py
@@ -89,26 +89,30 @@ class MetricViewer:
                     meta = {}
 
                 _, line = os.path.split(viewer_root)
-                metric = {"line": line, "num": i}
-
-                if "created_at" in meta:
-                    metric["created_at"] = pendulum.parse(meta["created_at"])
-                    if "saved_at" in meta:
-                        metric["saved"] = pendulum.parse(
-                            meta["saved_at"]
-                        ).diff_for_humans(metric["created_at"])
-
-                if "params" in meta:
-                    metric.update(meta["params"])
-                if "tags" in meta:
-                    metric["tags"] = meta["tags"]
-                if "comments" in meta:
-                    metric["comment_count"] = len(meta["comments"])
-                if "links" in meta:
-                    metric["link_count"] = len(meta["links"])
                 if "metrics" in meta:
+                    # Need to generate new metric each time
                     for m in meta["metrics"]:
-                        metric[m["name"]] = m.get("value")
+                        metric = {"line": line, "num": i}
+
+                        if "created_at" in meta:
+                            metric["created_at"] = pendulum.parse(meta["created_at"])
+                            if "saved_at" in meta:
+                                metric["saved"] = pendulum.parse(
+                                    meta["saved_at"]
+                                ).diff_for_humans(metric["created_at"])
+
+                        if "params" in meta:
+                            metric.update(meta["params"])
+                        if "tags" in meta:
+                            metric["tags"] = meta["tags"]
+                        if "comments" in meta:
+                            metric["comment_count"] = len(meta["comments"])
+                        if "links" in meta:
+                            metric["link_count"] = len(meta["links"])
+
+                        metric["name"] = m.get("name")
+                        metric["value"] =  m.get("value")
+
                         for key in m.keys():
                             if key not in ("name", "value", "created_at"):
                                 if m[key] is not None:
@@ -116,7 +120,7 @@ class MetricViewer:
 
                         self._metrics.append(metric)
                 else:
-                    self._metrics.append(metric)
+                    self._metrics.append({"line": line, "num": i})
         self.table = pd.DataFrame(self._metrics)
 
     def __repr__(self) -> str:
