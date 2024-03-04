e52b104babbaaeb73ff8d810e9452b479a2436a7
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-03-04 21:59:48 +0300

    Draft dataline

--- cascade/lines/data_line.py
+++ cascade/lines/data_line.py
@@ -1,12 +1,23 @@
-from typing import Any
+from typing import Any, Dict, Literal, Type
 
-from cascade.base import PipeMeta
+from ..base import PipeMeta
+from ..data.dataset import Dataset
+from .disk_line import DiskLine
 
-from ..base import TraceableOnDisk
-from .line import Line
 
+class DataLine(DiskLine):
+    def __init__(
+        self,
+        root: str,
+        ds_cls: Type[Any] = Dataset,
+        *args: Any,
+        meta_prefix: Dict[Any, Any] | str | None = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            root, item_cls=ds_cls, *args, meta_prefix=meta_prefix, **kwargs
+        )
 
-class DataLine(TraceableOnDisk, Line):
     def reload(self) -> None:
         pass
 
@@ -16,5 +27,15 @@ class DataLine(TraceableOnDisk, Line):
     def save(self, obj: Any, only_meta: bool = False) -> None:
         pass
 
+    def load_obj_meta(self, pathspec: str) -> PipeMeta: ...
+
+    def __getitem__(self, num: int) -> Any:
+        pass
+
+    def __repr__(self) -> str:
+        return f"DataLine of {len(self)} versions of {self._item_cls}"
+
     def get_meta(self) -> PipeMeta:
-        return super().get_meta()
+        meta = super().get_meta()
+        meta[0].update({"type": "data_line"})
+        return meta
