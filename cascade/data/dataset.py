3fe66690ef18207fcfaeabb4e10de645ef850020
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-06-16 00:42:27 +0300

    Attach data cards to datasets

--- cascade/data/dataset.py
+++ cascade/data/dataset.py
@@ -13,9 +13,20 @@ limitations under the License.
 
 import warnings
 from abc import abstractmethod
-from typing import Any, Generator, Generic, Iterable, Iterator, Sequence, Sized, TypeVar
+from typing import (
+    Any,
+    Generator,
+    Generic,
+    Iterable,
+    Iterator,
+    Optional,
+    Sequence,
+    Sized,
+    TypeVar,
+)
 
 from ..base import PipeMeta, Traceable
+from .data_card import DataCard
 
 T = TypeVar("T", covariant=True)
 
@@ -29,11 +40,12 @@ class BaseDataset(Generic[T], Traceable):
     cascade.base.Traceable
     """
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, data_card: Optional[DataCard] = None, *args: Any, **kwargs: Any) -> None:
+        self._data_card = data_card
         super().__init__(*args, **kwargs)
 
     @abstractmethod
-    def __getitem__(self, index: Any) -> T:...
+    def __getitem__(self, index: Any) -> T: ...
 
     def get_meta(self) -> PipeMeta:
         """
@@ -46,6 +58,8 @@ class BaseDataset(Generic[T], Traceable):
         """
         meta = super().get_meta()
         meta[0]["type"] = "dataset"
+        if self._data_card is not None:
+            meta[0]["data_card"] = self._data_card.to_dict()
         return meta
 
 
