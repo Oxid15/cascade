cc228198a2837c3da098112b7db3380d43d9eb7a
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-06-26 23:38:44 +0300

    Fix didn't use name in the process of linking

--- cascade/base/traceable.py
+++ cascade/base/traceable.py
@@ -14,17 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from dataclasses import dataclass, asdict
-from getpass import getuser
-import os
 import glob
+import os
 import socket
 import warnings
-from typing import Dict, Union, Any, Literal, Iterable, Optional
-import pendulum
+from dataclasses import asdict, dataclass
 from datetime import datetime
+from getpass import getuser
+from typing import Any, Dict, Iterable, Literal, Optional, Union
 
-from . import PipeMeta, Meta, default_meta_format, supported_meta_formats, MetaIOError
+import pendulum
+
+from . import Meta, MetaIOError, PipeMeta, default_meta_format, supported_meta_formats
 
 DO_NOT_UPDATE = ["created_at"]
 
@@ -335,6 +336,7 @@ class Traceable:
                 else:
                     obj_meta = obj.get_meta()
                     meta = [{
+                        "name": name,
                         "type": obj_meta[0].get("type"),
                         "description": obj_meta[0].get("description"),
                         "tags": obj_meta[0].get("tags"),
