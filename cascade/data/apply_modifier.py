fb1540a0371d05de50d66cff70f317df1df34443
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2024-08-21 21:42:22 +0300

    Write doc

--- cascade/data/apply_modifier.py
+++ cascade/data/apply_modifier.py
@@ -46,6 +46,10 @@ class ApplyModifier(Modifier[T]):
         func: Callable
             A function to be applied to every item of a dataset -
             each `__getitem__` would call `func` on an item obtained from a previous dataset
+        p: Optional[float], by default None
+            The probability [0, 1] with which to apply `func`
+        seed: Optional[int], by default None
+            Random seed is used when p is not None
 
         Examples
         --------
