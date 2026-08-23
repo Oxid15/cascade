34d3b3e03f605a9e235e4f49f1a81b5672c7648b
Ilia Moiseev <ilia.moiseev.5@yandex.ru>
2026-08-23 16:08:46 +0300

    Remove debug print

--- cascade/cli/run.py
+++ cascade/cli/run.py
@@ -372,7 +372,6 @@ def run(
             base_cfg = load_config(base)
 
             check_result = can_safely_replace(cfg_dict, base_cfg)
-            print(check_result, f)
             if not check_result.ok and not f:
                 raise Exception(
                     f"Cannot initialize the config in the file using base from {base}."
